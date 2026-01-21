eb93f95e75ef090460490347a8f50b56349aee1f
coder0606-op <iamayush616@gmail.com>
2026-01-21 20:58:31 +0530

    done

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,9 +3,6 @@ import { register, login } from "../controllers/authController.js";
 
 const router = Router();
 
-/**
- * AUTH ROUTES
- */
 router.post("/register", register);
 router.post("/login", login);
 
